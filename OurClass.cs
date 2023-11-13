7d140e539ccfe3517db92593a937fc154f5658d8
samiksha12n <samikshaunesarkar@gmail.com>
2023-11-13 15:36:46 +0530

    Methods and Parameters

---
+++ OurClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment2MethodsParameters
+{
+    internal class OurClass
+    {
+        public void display(int workingHours =8, int workingdays =8, int projecthandel=0,int extra=0)
+        {
+            int result = workingHours * workingdays * 100 + projecthandel * 3000 + extra * 2000;
+            Console.WriteLine("Monthly Salary of employee: \t" +result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6Assignment2MethodsParameters
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OurClass Hr = new OurClass();
+            Hr.display(workingHours: 8, workingdays: 30);
+            OurClass Admin = new OurClass();
+            Admin.display(workingHours: 8, workingdays: 30, projecthandel: 3);
+            OurClass softwareDeveloper = new OurClass();
+            softwareDeveloper.display(workingHours: 8, workingdays: 30, projecthandel: 4, extra: 5);
+            Console.ReadKey();
+        }
+    }
+}
